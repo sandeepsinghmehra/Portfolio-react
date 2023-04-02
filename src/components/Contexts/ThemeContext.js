0da62ab031259601c4191df7a904eb29933d2191
Sandeep Singh Mehra <53959751+sandeepsinghmehra@users.noreply.github.com>
2023-04-02 16:27:48 +0530

    delet src

--- src/components/Contexts/ThemeContext.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export const initialThemeState = {
-  theme: "dark",
-  setTheme: () => null
-};
-
-const ThemeContext = React.createContext(initialThemeState);
-export default ThemeContext;
--- src/components/Footer.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export const Footer = () => {
-    return (
-        <section className="footer">
-            Developed By <b style={{color:"red"}}>Sandeep</b> &copy; 2021
-        </section>
-    )
-}
